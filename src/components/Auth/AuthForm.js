db8a4ed509019337db0ac5f105f07df0eab2d3b9
roshinys <roshinchaya@gmail.com>
2023-07-15 11:48:01 +0530

    added signup form to firebase

---
+++ src/components/Auth/AuthForm.js
@@ -0,0 +1,76 @@
+import { useState, useRef } from "react";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    if (isLogin) {
+    } else {
+      const email = emailRef.current.value;
+      const password = passwordRef.current.value;
+      console.log(email, password);
+      const response = await fetch(process.env.REACT_APP_FIREBASE_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        //
+      } else {
+        const data = await response.json();
+        let errorMessage = "Authentication Failed";
+        if (data && data.error && data.error.message) {
+          errorMessage = data.error.message;
+        }
+        alert(errorMessage);
+      }
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required ref={passwordRef} />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && <button type="submit">Submit</button>}
+          {isLoading && <p>Sending Request</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
